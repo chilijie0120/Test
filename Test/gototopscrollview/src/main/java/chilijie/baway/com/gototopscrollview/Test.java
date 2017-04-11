60cf19663b8a49c22a01f0126029eef15f4cb85c
chilijie0120 <583837538@qq.com>
2017-04-11 17:52:19 +0800

    第一次

---
+++ Test/gototopscrollview/src/main/java/chilijie/baway/com/gototopscrollview/Test.java
@@ -0,0 +1,10 @@
+package chilijie.baway.com.gototopscrollview;
+
+/**
+ * Created by MacBook- on 2017/4/11.
+ */
+public class Test {
+    public double addData(double num1,double num2){
+        return num1+num2;
+    }
+}
